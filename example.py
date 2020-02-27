020553acdd49fea049d9aead24e5c2f10ecaf8ef
Yahir Vlatko Kozel <49602261+RealAtom@users.noreply.github.com>
2020-02-27 11:04:01 -0300

    Update example.py

--- example.py
+++ example.py
@@ -1,1 +1,17 @@
+import query
 
+def decode_int(string):
+    result = 0
+    for n, c in enumerate(string):
+        if isinstance(c, str):
+            c = ord(c)
+        result |= c << (8 * n)
+    return result
+  
+  def GetPlayersCount(address, port):
+    req = query.SendQueryRequest(address, port, 'i')
+
+    if req == 0:
+        return 'Offline'
+    else:
+        return f'{decode_int(req[1:3])}/{decode_int(req[3:5])}'
